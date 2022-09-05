c7ef04ab5e8a2721839611c2473d500b54c1b7e5
NexeiiOwO <112838325+NexeiiOwO@users.noreply.github.com>
2022-09-05 04:36:56 +0200

    Create aboutme.pl

---
+++ aboutme.pl
@@ -0,0 +1,11 @@
+- 👋 Hi, I’m @NexeiiOwO
+- 👀 I’m interested in Furry, Arts of Cruise ship & Others 
+I love a Arts 
+- 🌱 I’m currently learning run faster 👍
+- 💞️ I’m looking to collaborate on MatusOwO
+- 📫 How to reach me JAK Idk XD
+
+<!---
+NexeiiOwO/NexeiiOwO is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
